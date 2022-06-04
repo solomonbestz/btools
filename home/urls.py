35416421e3b40bb34e4a3aa603d950453f279cd5
Solomon Bestz <kingsolodawarrior@gmail.com>
2022-06-04 14:28:29 +0100

    mobile next view

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,10 @@
+from unicodedata import name
 from django import views
 from django.urls import path, include
 from home import views
 
 urlpatterns = [
     path('', views.home, name="home"),
-    path('gdrive', views.g_drive, name="gdrive")
+    path('gdrive', views.g_drive, name="gdrive"),
+    path('secondpage', views.secondpage, name="secondpage")
 ]
--- home/views.py
+++ home/views.py
@@ -3,6 +3,9 @@ from django.shortcuts import render, HttpResponse
 def home(request):
     return render(request, "index.html")
 
+def secondpage(request):
+    return render(request, "mobilehome.html")
+
 def g_drive(request):
     return render(request, "drive.html")
 
