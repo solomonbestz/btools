573661a6b7eaa7577e79bb18217b8e3a9155aa6d
Solomon Bestz <kingsolodawarrior@gmail.com>
2022-06-18 13:47:57 +0100

    youtube video download script

---
+++ static/scripts/videodownloadlink.js
@@ -0,0 +1,10 @@
+$(".button_click").click(function(){
+    var link = $(".link").val();
+    var format = $(".format").children("option:selected").val();
+    var src = "" + link + "=" + format + "";
+    download(link, format)
+});
+
+function download(link, format){
+    $('.download_button').html('<iframe style="height:50px; border:none; overflow:hidden;" src="https://loader.to/api/button/?url='+ link + '&f=' + format + '"></iframe>')
+}
